b5e20486d8b4e48dee3fb38a5fc2de86a3efdea4
rhk <rhk@thehemanth.com>
2022-07-31 14:24:11 +0800

    created front page for rhk

---
+++ src/components/Banner/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {BackGroundImage} from "./BannerElements";
+
+const Banner = () => {
+    return (
+        <BackGroundImage/>
+        )
+}
+
+export default Banner
